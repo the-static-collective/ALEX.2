02c44a60a02ef974f424bd0bab2e9a7695c4bc16
the static collective <joyvolcanomusic@gmail.com>
2026-08-28 14:09:48 -0500

    feat: export LOADINSTEAD m0 contract

---
+++ loadout_runtime/__init__.py
@@ -0,0 +1,19 @@
+from loadout_runtime.loadinstead import (
+    DOOR_SCHEMA,
+    LOADINSTEAD_M0_PROFILE,
+    ROUTE_BIT_SCHEMA,
+    ROUTE_PROPOSAL_SCHEMA,
+    route_bit,
+    validate_door,
+    validate_route_bit,
+)
+
+__all__ = [
+    "DOOR_SCHEMA",
+    "LOADINSTEAD_M0_PROFILE",
+    "ROUTE_BIT_SCHEMA",
+    "ROUTE_PROPOSAL_SCHEMA",
+    "route_bit",
+    "validate_door",
+    "validate_route_bit",
+]
